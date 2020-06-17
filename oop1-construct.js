ef4b07a935e7e4584ae330cce86f94a966402f26
Gowtham Katta <gowthamkatta@gmail.com>
2020-06-17 16:27:26 -0500

    Finished exercise 1, progress on exercise 2

--- oop1-construct.js
+++ oop1-construct.js
@@ -2,80 +2,190 @@
 
 //your code here...
 
+class WhyClass {
+  constructor(purpose) {
+    this.purpose =
+      'Classes are the blueprints used to create objects, provide initial values for states, and implementations of behavior';
+  }
 
+  explain() {
+    console.log(this.purpose);
+  }
+
+  pieces() {
+    console.log(
+      'constructors, objects (like properties and methods), the this keyword, and the new keyword'
+    );
+  }
+}
+
+const NewClass = new WhyClass();
+NewClass.explain();
+NewClass.pieces();
 
 /*2. You are exploring the rainforests of the Amazon.  You have observed many different types of wildlife on your excursion.  To jog your memory, some different types of wildlife that you observed are found in the following URL : https://www.ietravel.com/blog/amazon-rainforest-animals-beginners-guide-21-species.  Feel free to use other resources online if you wish.
 
 Create a class that can categorize at least 6 of the creatures that you recall observing the Amazon.  Each creature should have 3 unique characteristics, and 2 unique actions that it can do. */
 
-
 //your code here...
 
+class Amazon {
+  constructor(name, classification, color, actions) {
+    this.name = name;
+    this.classification = classification;
+    this.color = color;
+    this.actions = actions;
+  }
+  z;
 
+  about() {
+    console.log(`One of the many animals in the Amazon rainforests is the ${this.name}, a ${this.color}-colored ${this.classification}.`);
+  }
 
-
-/*3. Convert the following Object Literals into a general class called Shape, and instantiate the class to generate the object literals
-*/
-
-
-class Shape {
-    //your code here...
+  actions() {
+    console.log(`One unique action the ${this.name} can do is ${this.actions[0]} while another is ${this.actions[1]}.`);
+  }
 }
 
+const dolphin = new Amazon('Amazon River Dolphin', 'mammal', 'pink', ["move its' neck left and right", 'feed on small fish, crabs, and turtles']);
+const capybara = new Amazon('Capybara', 'mammal', 'brown', ['eat up to 8 pounds of grass per day', 'reside near the swampy areas']);
+const jaguar = new Amazon('Jaguar', 'mammal', 'yellow', ['swim', 'climb trees']);
+const owl = new Amazon('Spectacled Owl', 'bird', 'dark', ['stay active at night', 'hunt by swooping down from their roost']);
+const toucan = new Amazon('Toco Toucan', 'bird', 'black and white', ['look awkward while it flies', 'socialize with other toucans']);
+const caiman = new Amazon('Black Caiman', 'reptile', 'darl', ['hunt any land-based or river-dwelling animal', 'even attack Pumas or Jaguars']);
 
-const triangle = {
-    name : "triangle",
-    sides : [4,7,7], //lengths of each side
-    base : null,
-    height : null, //determine this height using basic geometry.  You may need to research this one.
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${.5 * this.base * this.height}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(this.base) + (this.sides[1] + this.sides[2]) }`)
+dolphin.about();
+owl.actions();
+
+/*3. Convert the following Object Literals into a general class called Shape, and instantiate the class to generate the object literals
+ */
+
+class Shape {
+    constructor(name, sides, base, height, length, width, radius) {
+        this.name = name;
+        this.sides = sides;
+        this.base = base;
+        this.height = height;
+        this.length = length;
+        this.width = width;
+        this.radius = radius;
+    }
+
+
+    calcHeight() {
+        if (this.name === 'triangle'){
+            return Math.sqrt(Math.pow(this.sides[1], 2) - Math.pow(this.base, 2)).toFixed(2);
+        };
+    }
+
+    calcArea() {
+        if (this.name === 'triangle'){
+            console.log(`The area of a ${this.name} is ${.5 * this.base * this.height}.`);
+        }
+        else if (this.name === 'rectangle') {
+            console.log(`The area of a ${this.name} is ${this.length * this.width}.`)
+        }
+        else if (this.name === 'circle'){
+            console.log(`This area of a ${this.name} is ${(Math.PI * Math.pow(this.radius, 2)).toFixed(2)}`)
+        }
+    }
+
+    calcPerimeter() {
+        if (this.name === 'triangle') {
+            console.log(`The perimeter of a ${this.name} is ${(this.base) + (this.sides[1]) + this.sides[2]}`)
+        }
+        else if (this.name === 'rectangle') {
+            console.log(`The perimeter of a ${this.name} is ${(this.length * 2) + (this.width * 2)}`)
+        }
+    }
+
+    calcCircumference() {
+        if (this.name === 'circle') {
+            console.log(`The circumference of a ${this.name} is ${(Math.PI * this.radius * 2).toFixed(2)}`)
+        }
     }
 }
 
-triangle.base = triangle.sides[0];  //base is the 1st side in the triangle.sides array.
+
+const newTriangle = new Shape("triangle", [4,7,7], null, null, null, null, null);
+newTriangle.base = newTriangle.sides[0];
+newTriangle.height = newTriangle.calcHeight();
+
+console.log(newTriangle)
+newTriangle.calcArea()
+newTriangle.calcPerimeter()
+
+const newRectangle = new Shape ('rectangle', 4, null, null, 2, 5, null);
+
+console.log(newRectangle)
+newRectangle.calcArea()
+newRectangle.calcPerimeter()
+
+const newCircle = new Shape('circle', 1, null, null, null, null, 5);
+
+console.log(newCircle)
+newCircle.calcArea()
+newCircle.calcCircumference()
+
+
+
+const triangle = {
+  name: 'triangle',
+  sides: [4, 7, 7], //lengths of each side
+  base: null,
+  height: null, //determine this height using basic geometry.  You may need to research this one.
+  calcArea: function () {
+    console.log(`${this.name}'s area is calculated to be : ${0.5 * this.base * this.height}`);
+  },
+  calcPerimeter: function () {
+    console.log(`${this.name}'s perimeter is calculated to be : ${this.base + (this.sides[1] + this.sides[2]) }`);
+  },
+};
+
+
+triangle.base = triangle.sides[0]; //base is the 1st side in the triangle.sides array.
 triangle.height = 6.71; //determine the height using basic geometry.  How do you calculate the height of a triangle with 2 equal sides?  If you use the formula to caluclate the height, this is a bonus.  If you hard code the correct value here, that will be sufficient, but no bonus :(
 
 console.log(triangle);
 triangle.calcArea();
 triangle.calcPerimeter();
 
+
 const rectangle = {
-    name : "rectangle",
-    sides : 4,
-    length : 2,
-    width : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${this.length * this.width}`);
-    },
-    calcPerimeter : function() {
-        console.log(`${this.name}'s perimeter is calculated to be : ${(2 * this.length) + (2 * this.width) }`)
-    }
-}
+  name: 'rectangle',
+  sides: 4,
+  length: 2,
+  width: 5,
+  calcArea: function () {
+    console.log(`${this.name}'s area is calculated to be : ${this.length * this.width}`);
+  },
+  calcPerimeter: function () {
+    console.log(`${this.name}'s perimeter is calculated to be : ${2 * this.length + 2 * this.width}`);
+  },
+};
 
 console.log(rectangle);
 rectangle.calcArea();
 rectangle.calcPerimeter();
 
+
+const newCircle = new Shape("Circl")
+
 const circle = {
-    name : "circle",
-    sides : 1,
-    radius : 5,
-    calcArea : function() {
-        console.log(`${this.name}'s area is calculated to be : ${(Math.PI * this.radius**2).toFixed(2)}`);
-    },
-    calcCircumference : function() {
-        console.log(`${this.name}'s circumference is calculated to be : ${(2 * Math.PI * this.radius).toFixed(2)}`)
-    }
-}
+  name: 'circle',
+  sides: 1,
+  radius: 5,
+  calcArea: function () {
+    console.log(`${this.name}'s area is calculated to be : ${(Math.PI * this.radius ** 2).toFixed(2)}`);
+  },
+  calcCircumference: function () {
+    console.log(`${this.name}'s circumference is calculated to be : ${(2 * Math.PI * this.radius).toFixed(2)}`);
+  },
+};
 
 console.log(circle);
 circle.calcCircumference();
 circle.calcArea();
-
 
 /*********************************************** 
 Bonus Exercises:
@@ -88,19 +198,19 @@ Bonus Exercises:
 */
 
 class Earth {
-    name;
-    planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
+  name;
+  planetNum; //distance from the sun.  1-mercury, 2-venus, 3-earth, ...
 
-    constructor(name,num) {
-        this.name = name;
-        this.planetNum = num;
-    }
-
+  constructor(name, num) {
+    this.name = name;
+    this.planetNum = num;
+  }
 }
 
-const earth = new Earth('earth',3);
+/*
+const earth = new Earth('earth', 3);
 console.log(earth);
+*/
 
-
-
-
+const earth = {name: 'earth', planetNum: 3}
+console.log(earth)
--- oop2-encapsulate-inherit.js
+++ oop2-encapsulate-inherit.js
@@ -7,13 +7,44 @@
     -Prevent the properties from being accessed by outside code, using the encapsulation concept of "private variables".  Create methods that return the values of these private variables in case other pieces of the code need to access the data of the 4 classes.  Also create methods that allow outside code to "set" new values for the properties on the classes, without directly manipulating the values themselves. 
 
     //your code here...
+*/
+
+class Book {
+    #author = "Matthew Syed";
+    #publisher = "Penguin Publishing Group";
+
+    constructor() {
+        this.#author = "Matthew Syed";
+        this.#publisher = "Penguin Publishing Group";
+    }
+
+    getAuthor() {
+        return this.#author;
+    }
+
+    getPublisher() {
+        return this.#publisher;
+    }
+
+    setAuthor(fill1) {
+        this.#author = fill1;
+    }
+
+    setPublisher(fill2) {
+        this.#publisher = fill2;
+    }
+
+}
+
+class Author {
+    #name = "Matthew Syed";
+    #books = ""
+}
 
 
 
 
-
-
-
+/* 
 2. Create the following classes:
     - Umbrella class.  It should have a an organization name.  It should only have a single instance.  Umbrella is the "Parent Organization" e.g. Allegis Group
     - Company class. It should have a compnay name and # of employees.  Create at least 3 different companies that are children of the Umbrella Organization.  (e.g. TEKsystems, Aerotek, etc.)
@@ -25,21 +56,18 @@
 
 
     //your code here...
-
-
-
+ */
 
 /****************************************************************************************************************************************************************************************   
 Bonus Exercise:
 
+/* 
 3. Building on Exercise 1, Do the following : 
 - Create a Bookstore class.  It should contain a collection of various Book Instances.
 - Each Book Instance should contain the Instances of the relavant Authors, Publishers, and Reviews.
 - Each Publisher should contain Instances of the Authors that are affiliated with the relavant Publisher.  
 
 Each Book instance should contain instances of the Authors, the Publishers, and the Reviews pertinent to each book.  This exercise is using the advanced encapsulation concept of "composition", reflecting a "HAS A" relationship.
-*/
-
-
 
     //your code here...
+ */
